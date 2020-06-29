356f3ba32a500e131c4c3ab8c2542879d76c074b
James Hawley <iliketofeelcheesetootoo@yahoo.com>
2020-06-29 10:10:24 -0500

    Made additions to convert to one Angular module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
+// Angular imports.
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
+// Ionic imports.
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+// This app's imports.
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  // 'declarations' brings in the stuff that I make.
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  // 'imports' brings in the stuff I bring in from elsewhere.
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: '**', redirectTo: 'home', pathMatch: 'full' }
+    ]),
+  ],
   providers: [
     StatusBar,
     SplashScreen,
